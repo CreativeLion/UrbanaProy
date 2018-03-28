f8f8d03e1a7791084b286307dae3e8b6be326324
03Jacg <37789084+03Jacg@users.noreply.github.com>
2018-03-27 21:40:54 -0600

    adding some files

--- index.php
+++ index.php
@@ -11,5 +11,24 @@
   <body>
     <label>Hello World Jesus editado desde la rama jacg</label>
     <button class="btn btn-primary">Test button</button>
+
+    <div class="row">
+      <button type="button" name="button" class=""></button>
+      <button class="" name="button"></button>
+      <a href="#" class=""></a>
+    </div>
+
+    </div>
+
+    </div>
+
+
+    <script type="text/javascript" src="Js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+            crossorigin="anonymous"></script>
   </body>
 </html>
