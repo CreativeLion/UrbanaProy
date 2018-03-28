3f31fbc2246da5162a315fc50c9bfd9d4437f660
03Jacg <37789084+03Jacg@users.noreply.github.com>
2018-03-27 23:17:20 -0600

    labels

--- index.php
+++ index.php
@@ -13,7 +13,6 @@
         <button class="btn btn-primary">Test button</button>
 
 
-
         <script type="text/javascript" src="Js/bootstrap.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
                 integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
