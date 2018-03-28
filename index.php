db356ab52356e9d430670e0d70e34a0ba4bce908
03Jacg <37789084+03Jacg@users.noreply.github.com>
2018-03-27 22:08:25 -0600

    commit

--- index.php
+++ index.php
@@ -1,34 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css"href="CSS/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="CSS/styles.css">
-    <title>Urbana</title>
-  </head>
-  <body>
-    <label>Hello World Jesus editado desde la rama jacg</label>
-    <button class="btn btn-primary">Test button</button>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" type="text/css"href="CSS/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="CSS/styles.css">
+        <title>Urbana</title>
+    </head>
+    <body>
+        <label>Hello World Jesus editado desde la rama jacg</label>
+        <button class="btn btn-primary">Test button</button>
 
-    <div class="row">
-      <button type="button" name="button" class=""></button>
-      <button class="" name="button"></button>
-      <a href="#" class=""></a>
-    </div>
+        <div class="row">
+            <button type="button" name="button" class=""></button>
+            <button class="" name="button"></button>
+            <a href="#" class=""></a>
+        </div>
 
-    </div>
+        </div>
 
-    </div>
+        </div>
 
 
-    <script type="text/javascript" src="Js/bootstrap.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-            crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        <script type="text/javascript" src="Js/bootstrap.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+                integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+                crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
             integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
             crossorigin="anonymous"></script>
-  </body>
+    </body>
 </html>
