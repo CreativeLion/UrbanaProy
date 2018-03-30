5c7179f8c832a8a0278f3f26200dd367aa53b213
03Jacg <37789084+03Jacg@users.noreply.github.com>
2018-03-29 20:51:00 -0600

    Cambios del jueves

---
+++ Extends/menu.php
@@ -0,0 +1,36 @@
+<nav class="container navbar navbar-overlay navbar-toggeable navbar-expand-lg navbar-dark sticky-top">
+
+    <a href="index.php" class="navbar-brand ml-lg-5">
+        <img src="Images/Logo.png" alt="Logotipo Urbana">
+    </a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+        <ul class="navbar-nav ml-lg-auto mr-lg-5">
+            <li class="nav-item">
+                <a class="nav-link text-center" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-center" href="#">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-center" href="#">Demo Reel</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-center" href="#">Team</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-center" href="#">Portafolio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-center" href="#">Testimonios</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-center" href="#">Contacto</a>
+            </li>
+        </ul>
+    </div>
+</nav>
