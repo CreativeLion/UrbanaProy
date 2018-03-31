04e6ce1601178b40a800bea18e2b7ab262f2dbdc
gloria27 <37825661+gloria27@users.noreply.github.com>
2018-03-31 17:48:33 -0600

    cambios de section

--- index.php
+++ index.php
@@ -45,9 +45,23 @@
             </div>
 
         </header>
-
-        <a href="#"class="btn btn-outline-success btn-lg">LEARN MORE</button>
-
+        <!--section about-->
+        <section id="about">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-3 "><img class="img-fluid" id="img1" src="Images/urbana-image1.png" alt="imagen1"></div>
+                    <div class="col-md-1"></div>
+                    <div class="col-md-7">
+                        <h3 class="mt-4">ABOUT</h3>
+                        <p class="mt-2 text-justify">Nosotros somos los expertos, los crativos, los que incursionamos para llevar
+                        nuevas sensaciones y experincias</p>
+                        <p>Amamos lo que hacemos, amamos las nuevas experiencias y los nuevos retos.
+                            Creamos experiencias, y a partir de ellas, somos Urbana.</p>
+                         <button class="btn" type="button" name="button">LEARN MORE</button> 
+                    </div>
+                </div>
+        </section>
 
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
