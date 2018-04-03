33022621d808903dc610218108815b332752c467
gloria27 <37825661+gloria27@users.noreply.github.com>
2018-04-03 13:43:25 -0500

    cambios 2 footer

--- Extends/footer.php
+++ Extends/footer.php
@@ -3,17 +3,17 @@
         <div class="row text-center text-md-left mt-3 pb-3">
             <div class="offset-1 col-lg-3">
                 <img class="img-fluid" src="Images/Logo.png" alt="Logotipo Urbana">
-                <p class="mt-4 text-white">lorem quis bibendum auctor,nisi elit consequat ipsum, nec sagittis sem nibh elit. Duis sed odio sit amet auctror a ornare odio non mauris vitae erat in elit.</p>
-                <p class="mt-5 text-white">HELP TERMS & CONDITION PRIVACY</p>
+                <p class="mt-4 text-white text-center">lorem quis bibendum auctor,nisi elit consequat ipsum, nec sagittis sem nibh elit. Duis sed odio sit amet auctror a ornare odio non mauris vitae erat in elit.</p>
+                <p class="mt-5 text-white text-center">HELP TERMS & CONDITION PRIVACY</p>
             </div>
             <div class="offset-1 col-lg-2">
-                <p class="mt-4 text-white text-center">OUR STUDIO</p>
-                <p class="text-white"><i class="fa fa-map-marker fa-1x" aria-hidden="true"></i> Ruko cucruk, JI Radio luar dalem jos No.12-13, Kalideres-Jakarta Barat 11480-Monterrey, México</p>
-                <p class="mt-4 text-white"><i class="fa fa-phone mr-3"></i> + 01 234 567 88</p>
-                <p class="mt-4 text-white">Copyright © 2018 Urbana</p>
+                <p class="mt-4 text-white text-center font-weight-bold">OUR STUDIO</p>
+                <p class="text-white text-center"><i class="fa fa-map-marker fa-1x" aria-hidden="true"></i> Ruko cucruk, JI Radio luar dalem jos No.12-13, Kalideres-Jakarta Barat 11480-Monterrey, México</p>
+                <p class="mt-4 text-white text-center"><i class="fa fa-phone mr-3"></i> + 01 234 567 88</p>
+                <p class="mt-4 text-white text-center">Copyright © 2018 Urbana</p>
             </div>
             <div class="offset-1 col-lg-3">
-                <p class="mt-4 text-white text-center">STAY IN TOUCH</p>
+                <p class="mt-4 text-white text-center font-weight-bold">STAY IN TOUCH</p>
                 <div class="form-group text-center">
                 <input id="in" class="form-control mt-4" id="subscribe" type="text" placeholder="Subscribe our newsletter">
                 <button class="btn btn-sm btn-outline-success px-4 mt-2"><i class="fa fa-location-arrow" aria-hidden="true"></i></button>
