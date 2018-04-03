5da00f560e329d70a61044026ce450fe588fe0ad
gloria27 <37825661+gloria27@users.noreply.github.com>
2018-04-03 13:38:23 -0500

    cambios de footer

--- Extends/footer.php
+++ Extends/footer.php
@@ -1,4 +1,34 @@
-<div class="">
+<footer id="latest" class="page-footer font-small pt-4">
+    <div class="container text-center text-md-left">
+        <div class="row text-center text-md-left mt-3 pb-3">
+            <div class="offset-1 col-lg-3">
+                <img class="img-fluid" src="Images/Logo.png" alt="Logotipo Urbana">
+                <p class="mt-4 text-white">lorem quis bibendum auctor,nisi elit consequat ipsum, nec sagittis sem nibh elit. Duis sed odio sit amet auctror a ornare odio non mauris vitae erat in elit.</p>
+                <p class="mt-5 text-white">HELP TERMS & CONDITION PRIVACY</p>
+            </div>
+            <div class="offset-1 col-lg-2">
+                <p class="mt-4 text-white text-center">OUR STUDIO</p>
+                <p class="text-white"><i class="fa fa-map-marker fa-1x" aria-hidden="true"></i> Ruko cucruk, JI Radio luar dalem jos No.12-13, Kalideres-Jakarta Barat 11480-Monterrey, México</p>
+                <p class="mt-4 text-white"><i class="fa fa-phone mr-3"></i> + 01 234 567 88</p>
+                <p class="mt-4 text-white">Copyright © 2018 Urbana</p>
+            </div>
+            <div class="offset-1 col-lg-3">
+                <p class="mt-4 text-white text-center">STAY IN TOUCH</p>
+                <div class="form-group text-center">
+                <input id="in" class="form-control mt-4" id="subscribe" type="text" placeholder="Subscribe our newsletter">
+                <button class="btn btn-sm btn-outline-success px-4 mt-2"><i class="fa fa-location-arrow" aria-hidden="true"></i></button>
+                </div>
+                <div class="text-center mt-4">
+                          <ul class="list-unstyled list-inline">
+                              <li class="list-inline-item"><a class="btn-floating btn-sm rgba-white-slight mx-1"><i class="fa fa-facebook" style="color:white"></i></a></li>
+                              <li class="list-inline-item"><a class="btn-floating btn-sm rgba-white-slight mx-1"><i class="fa fa-twitter" style="color:white"></i></a></li>
+                              <li class="list-inline-item"><a class="btn-floating btn-sm rgba-white-slight mx-1"><i class="fa fa-google-plus" style="color:white"></i></a></li>
+                              <li class="list-inline-item"><a class="btn-floating btn-sm rgba-white-slight mx-1"><i class="fa fa-linkedin" style="color:white"></i></a></li>
+                          </ul>
+                </div>
 
+            </div>
+        </div>
 
-</div>
+    </div>
+</footer>
